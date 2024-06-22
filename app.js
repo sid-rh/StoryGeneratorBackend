feabaad83fd5c2e7ee5f6aedf65564045d746aa0
sid-rh <siddhanth.rh@gmail.com>
2024-06-22 14:51:17 +0530

    commit

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ const userRoutes=require('./routes/userRoutes');
 const storyRoutes=require('./routes/storyRoutes');
 
 const app=express();
+app.use(cookieParser());
 app.use(cors(
     {
         credentials:true,
-        origin:["https://storygeneratorfrontend.onrender.com","http://localhost:3000"]
+        origin:"http://localhost:3000"
     }
 ));
-app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.json());
